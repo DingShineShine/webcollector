700ddc66d681cf37fcac06010995c35126d1c320
丁光辉 <785508803@qq.com>
2018-05-31 12:18:08 +0800

    添加环绕通知

--- src/main/java/com/ding/webcollector_boot/WebcollectorBootApplication.java
+++ src/main/java/com/ding/webcollector_boot/WebcollectorBootApplication.java
@@ -3,12 +3,14 @@ package com.ding.webcollector_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class WebcollectorBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebcollectorBootApplication.class, args);
